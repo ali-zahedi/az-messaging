e769d1500c7a17a5669673be7cf2f46ae954c4bc
Ali Zahedigol <ali.zahedigol@gmail.com>
2021-04-14 19:55:32 +0300

    fix: support white list and black list for countries

--- azmessaging/readers/smsconfig.py
+++ azmessaging/readers/smsconfig.py
@@ -1,9 +1,33 @@
+import pytz
+
+
 class SMSConfig:
-    def __init__(self, default, priorities, service_providers):
+    def __init__(self, default, priorities, service_providers, white_list_countries, black_list_countries):
         self.default_service_provider = default
         self.priorities_service_provider = priorities
         self.service_providers = service_providers
+        """
+        white list
+        """
+        if white_list_countries == '__all__':
+            wlc = set(pytz.country_names)
+        elif white_list_countries == '__none__' or not white_list_countries:
+            wlc = set()
+        else:
+            wlc = set(filter(None, [x.strip().upper() for x in white_list_countries.split(',')]))
+        """
+        black list
+        """
+        if black_list_countries == '__all__':
+            blc = set(pytz.country_names)
+        elif black_list_countries == '__none__' or not black_list_countries:
+            blc = set()
+        else:
+            blc = set(filter(None, [x.strip().upper() for x in black_list_countries.split(',')]))
+
+        self.white_list_countries = wlc - blc
 
     default_service_provider: str
     priorities_service_provider: [str]
     service_providers: dict
+    white_list_countries: [str]
